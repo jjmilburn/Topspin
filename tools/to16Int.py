957765651bb7537687514ef4b20a9754481b991b
Josh Milburn <joshm@angazadesign.com>
2014-08-07 21:24:28 -0700

    Initial commit, converts 12 bit samples to 16 bit
    Purpose: Get 12 bit (0-4095) from ADC readings, and convert to
    16 bit for debugging

---
+++ tools/to16Int.py
@@ -0,0 +1,24 @@
+#Josh Milburn
+#git - jjmilburn / jjmilburn@gmail.com
+#tool for converting 12 bit csv files to 16 bit
+#used for debugging samples from spark core serial output
+
+import csv
+import struct
+
+#import the csv file containing 0 to +4095 values from ADC
+twelveBitSamplesFile = open('samples12bit.csv', "r")
+twelveBitSamplesReader = csv.reader(twelveBitSamplesFile)
+
+sixteenBitSamplesBin = []
+
+#convert these to 16 bit ('short', or 'h') and write back to file
+with open('16bit.raw', 'wb') as f:
+    for sample in twelveBitSamplesReader:
+        #full scale and integer conversion -
+        scaledInt = 8*(int(sample[0])-4095)
+        packedInt = struct.pack('h',int(sample[0]))
+        f.write(packedInt)
+
+#to read these as 'full scale', import as unsigned 16 bit
+
